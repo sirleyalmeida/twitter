f4fe211adb09f39c9fd9dcc0c582c03694325e9c
sirleyalmeida <sirleyas90@gmail.com>
2019-02-20 23:27:57 -0300

    twitter faltando a expansão da caixa de texto

--- twitter.js
+++ twitter.js
@@ -1,13 +1,13 @@
 function tweet() {
-    let msgTweet = document.querySelector("textarea").value;
+    let msgTweet = document.querySelector("[contenteditable]").innerText.replace("\n", "<br>");
     let tweetHour = hour();
-    let msgTweetOut = document.getElementById("tweetOut").innerHTML = msgTweet + "\n" + tweetHour;
+    let msgTweetOut = document.getElementById("tweetOut").innerHTML = msgTweet + " " + tweetHour;
 }
 let btn = document.getElementById("tweetButton");
 btn.addEventListener("click", tweet);
 
 function charactersCounter() {
-    let msgTweet = document.getElementById("comments").value;
+    let msgTweet = document.querySelector("[contenteditable]").innerText;
     let charactersTypeds = msgTweet.length;
     let charactersLimit = 140;
     let charactersRemaining = charactersLimit - charactersTypeds;
@@ -18,13 +18,41 @@ function charactersCounter() {
         document.getElementById("counter").style.color = "#FF0000";
     }
     document.getElementById("counter").innerHTML = charactersRemaining;
-
 }
+let commentsTweet = document.getElementById("comments");
+commentsTweet.addEventListener("keyup", charactersCounter);
+
+function btnDisabled() {
+    let msgTweet = document.querySelector("[contenteditable]").innerText;
+    let charactersTypeds = msgTweet.length;
+
+    if (msgTweet === null && msgTweet === "") {
+        document.getElementById("tweetButton").disabled = true;
+    } else if (charactersTypeds > 140) {
+        document.getElementById("tweetButton").disabled = true;
+    } else {
+        document.getElementById("tweetButton").disabled = false;
+    }
+}
+let btnDisabledEnabled = document.getElementById("comments");
+btnDisabledEnabled.addEventListener("keyup", btnDisabled);
 
 function hour() {
     let data = new Date();
     let hora = data.getHours();
-    let min = data.getMinutes();
-    let str_hora = hora + ':' + min;
+    let min = data.getUTCMinutes();
+    let str_hora = hora + ":" + min;
     return str_hora;
 }
+
+function postTweet() {
+    let x = document.getElementById("postComments");
+    if (x.style.display === "none") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+let btnDisplayPost = document.getElementById("tweetButton");
+btnDisplayPost.addEventListener("click", postTweet);
